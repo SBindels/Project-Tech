1f79de5037df026e8b3b4f60fbaf7559ce5902c2
SBindels <bindelssjoerd@gmail.com>
2020-02-13 14:17:58 +0100

    .

--- Back-end/express_file_server/index.js
+++ Back-end/express_file_server/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>about page</h1>");
+});
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
